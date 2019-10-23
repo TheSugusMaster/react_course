bb1de31f0925fad07df166f9a0d12b03320e9dd4
TheSugusMaster <lmu.lukasmueller@gmail.com>
2019-10-23 11:01:36 +0200

    Adds delete method to App.tsx

--- crud-test/src/containers/App.tsx
+++ crud-test/src/containers/App.tsx
@@ -1,22 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import AutoForm from "../components/Auto/AutoForm";
 import AutoIndex from "../components/Auto/AutoIndex";
 import { Auto } from "../models/Auto";
 
 const App: React.FC = () => {
-  const autos: Auto[] = [
-    {id: 0, brand: 'Ford', model: 'Mondeo', year: '2000'},
-    {id: 1, brand: 'Opel', model: 'Corsa', year: '2002'},
-    {id: 2, brand: 'Citroen', model: 'C5 Break', year: '2002'}
-  ]
+  const [autos, setAutos] = useState<Auto[]>(
+    [
+      { id: 0, brand: 'Ford', model: 'Mondeo', year: '2000' },
+      { id: 1, brand: 'Opel', model: 'Corsa', year: '2002' },
+      { id: 2, brand: 'Citroen', model: 'C5 Break', year: '2002' }
+    ]
+  )
 
-  return (
-    <div className="App">
-      <AutoForm />
-      <AutoIndex autos={autos}/>
-    </div>
-  );
+const deleteAuto = (auto: Auto) => {
+  setAutos(autos.filter(a => a.id !== auto.id));
+}
+
+return (
+  <div className="App">
+    <AutoForm />
+    <AutoIndex autos={autos} onDelete={deleteAuto} />
+  </div>
+);
 }
 
 export default App;
