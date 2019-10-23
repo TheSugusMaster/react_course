3bc7be67c86c08d46f950f7464b7030ae0bc0dfa
TheSugusMaster <lmu.lukasmueller@gmail.com>
2019-10-23 10:53:31 +0200

    Adds basic implementation to AutoIndex.tsx

--- crud-test/src/components/Auto/AutoIndex.tsx
+++ crud-test/src/components/Auto/AutoIndex.tsx
@@ -1,7 +1,46 @@
 import React from "react";
+import { Auto } from "../../models/Auto";
 
-const AutoIndex: React.FC = () => {
-    return <div>AutoIndex</div>
+interface Props {
+    autos: Auto[],
+    onDelete: (auto: Auto) => void;
+}
+
+const AutoIndex: React.FC<Props> = (props) => {
+    const createDeleteAction = (auto: Auto) => {
+        return () => {
+            props.onDelete(auto);
+        }
+    }
+
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Brand</th>
+                    <th>Model</th>
+                    <th>Year</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                {props.autos.map(auto => {
+                    return (
+                        <tr key={auto.id}>
+                            <td>{auto.id}</td>
+                            <td>{auto.brand}</td>
+                            <td>{auto.model}</td>
+                            <td>{auto.year}</td>
+                            <td>
+                                <button onClick={createDeleteAction(auto)}>Delete</button>
+                            </td>
+                        </tr>
+                    )
+                })}
+            </tbody>
+        </table>
+    )
 }
 
 export default AutoIndex;
